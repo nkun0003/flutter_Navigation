1d49925b1b6e5eddba1c8ee59dfeef3cbb537856
nkun0003 <nkun0003@algonquinlive.com>
2024-11-22 17:46:26 -0500

    added appBar in home file

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,39 +6,45 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        // the top half with a background image
-        Expanded(
-          flex: 1,
-          child: Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('images/home_image.jpg'),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-        ),
-        // Bottom half with text
-        Expanded(
-          flex: 1,
-          child: Container(
-            color: Colors.black,
-            child: Center(
-              child: Transform.scale(
-                scale: 1.5, // Example transformation
-                child: Text(
-                  'Welcome to the Home Page',
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodyMedium, // here using my created theme text
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('App Two'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          // the top half with a background image
+          Expanded(
+            flex: 1,
+            child: Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('images/home_image.jpg'),
+                  fit: BoxFit.cover,
                 ),
               ),
             ),
           ),
-        ),
-      ],
+          // Bottom half with text
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Colors.black,
+              child: Center(
+                child: Transform.scale(
+                  scale: 1.5, // Example transformation
+                  child: Text(
+                    "Rocien Nkunga's Flutter App",
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyMedium, // here using my created theme text
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
